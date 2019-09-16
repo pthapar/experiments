c9e74ede540762b64089802b8f7baa2fd760de2c
Pankit Thapar <pankit.thapar@nutanix.com>
2019-09-15 22:46:42 -0700

    add worker genconfigs commands

--- k8s-from-scatch/roles/genconfigs.sh
+++ k8s-from-scatch/roles/genconfigs.sh
@@ -55,3 +55,45 @@ kubectl config set-cluster kubernetes-the-hard-way \
     --kubeconfig=${KUBECONFIG_DIR}/kube-scheduler.kubeconfig
 
   kubectl config use-context default --kubeconfig=${KUBECONFIG_DIR}/kube-scheduler.kubeconfig
+
+## Below commands on all worker nodes:
+# CERTS_DIR=~/k8s/certs/
+# CONFIGS_DIR=~/k8s/configs/
+# NODE_NAME=node[1,2,3]
+kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=${CERTS_DIR}/ca-k8s-apiserver.pem \
+    --embed-certs=true \
+    --server=https://127.0.0.1:443 \
+    --kubeconfig=${CONFIGS_DIR}/${NODE_NAME}.kubeconfig
+kubectl config set-credentials system:node:${NODE_NAME} \
+    --client-certificate=${CERTS_DIR}/cert-${NODE_NAME}.pem \
+    --client-key=${CERTS_DIR}/cert-${NODE_NAME}-key.pem \
+    --embed-certs=true \
+    --kubeconfig=${CONFIGS_DIR}/${NODE_NAME}.kubeconfig
+
+kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:node:${NODE_NAME} \
+    --kubeconfig=${CONFIGS_DIR}/${NODE_NAME}.kubeconfig
+
+kubectl config use-context default --kubeconfig=${CONFIGS_DIR}/${NODE_NAME}.kubeconfig
+
+# Each node
+kubectl config set-cluster kubernetes-the-hard-way \
+    --certificate-authority=${CERTS_DIR}/ca-k8s-apiserver.pem \
+    --embed-certs=true \
+    --server=https://127.0.0.1:443 \
+    --kubeconfig=${CONFIGS_DIR}/kube-proxy.kubeconfig
+
+  kubectl config set-credentials system:kube-proxy \
+    --client-certificate=${CERTS_DIR}/cert-k8s-proxy.pem \
+    --client-key=${CERTS_DIR}/cert-k8s-proxy-key.pem \
+    --embed-certs=true \
+    --kubeconfig=${CONFIGS_DIR}/kube-proxy.kubeconfig
+
+  kubectl config set-context default \
+    --cluster=kubernetes-the-hard-way \
+    --user=system:kube-proxy \
+    --kubeconfig=${CONFIGS_DIR}/kube-proxy.kubeconfig
+
+  kubectl config use-context default --kubeconfig=${CONFIGS_DIR}/kube-proxy.kubeconfig
